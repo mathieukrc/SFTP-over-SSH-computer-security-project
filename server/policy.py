da76594bda2b874ffe2db1491a254d75d7640c0d
thehungryian <bot.bacs@gmail.com>
2025-11-14 19:28:01 +0100

    Fixed RBAC, Implemented RBAC path helper, implemented composite rule which is wrong for now

--- server/policy.py
+++ server/policy.py
@@ -4,38 +4,61 @@ def load_RBAC_helper(user_roles_path, role_perms_path):
     opened_json = None
     opened_csv = None
     stored_csv = []
+    paths = []
     RBAC_policy = {}
     with open(user_roles_path,"r") as json_file, open(role_perms_path,"r") as csv_file:
         opened_json = json.load(json_file)
         opened_csv = csv.reader(csv_file, delimiter=',')
         for row in opened_csv:
             stored_csv.append(row)
+            paths.append(row[1])
         for item in opened_json.items():
+            if item[0] not in RBAC_policy:
+                RBAC_policy[item[0]] = {}
             for role in item[1]:
-                if item[0] in RBAC_policy:
-                    for row in stored_csv:
-                        if row[0] == role:
-                            resource = row[1]
-                            perms = row[2:]
-                            RBAC_policy[item[0]][role] = {resource:perms}
-                else:
-                    for row in stored_csv:
-                        if row[0] == role:
-                            resource = row[1]
-                            perms = row[2:]
-                            RBAC_policy[item[0]] = {role:{resource:perms}}
-    return RBAC_policy
-def RBAC(user, resource, action):
-    RBAC_policy = load_RBAC_helper("server/data/user_roles.json","server/data/role_perms.csv")
+                for row in stored_csv:
+                    if row[0] == role:
+                        resource = row[1]
+                        perms = row[2:]
+                        if role not in RBAC_policy[item[0]]:
+                            RBAC_policy[item[0]][role] = {}
+                        RBAC_policy[item[0]][role][resource] = perms
+    return RBAC_policy, paths
+"""
+THIS IS TERRIBLE PLEASE LET ME KNOW IF YOU FIGURE OUT A BETTER WAY
+"""
+def RBAC_path_helper(file_path,paths):
+    splitted_paths = []
+    for path in paths:
+        splitted_paths.append(path.split("/"))
+    splitted_file_path = file_path.split("/")
+    match_list = []
+    for path in splitted_paths:
+        match_list.append(len(set(splitted_file_path)&set(path)))
+    act_file_path = splitted_paths[match_list.index(max(match_list))]
+    act_string = "/"
+    return act_string.join(act_file_path)
+"""
+NEED TO IMPLEMENT ALLOWED AND DENIED PERMISSION OVERRIDES
+"""
+def RBAC(user, file_path, action):
+    RBAC_policy, paths = load_RBAC_helper("server/data/user_roles.json","server/data/role_perms.csv")
+    file_path = RBAC_path_helper(file_path,paths)
+    print(file_path)
     user_roles = RBAC_policy.get(user,None)
     if user_roles is None:
         return False
     for role in user_roles.keys():
         role_resources = user_roles.get(role,None)
         if role_resources is not None:
-            resource_perms = role_resources.get(resource,None)
+            resource_perms = role_resources.get(file_path,None)
             if resource_perms is not None:
-                if action in resource_perms:
+                resource_perms_dict = {
+                "read" : resource_perms[0],
+                "write" : resource_perms[1],
+                "delete" : resource_perms[2],
+            }
+                if resource_perms_dict.get(action,"N") == "Y":
                     return True
     return False
 
@@ -125,4 +148,12 @@ def DAC(user,file_path,action):
             else:
                 if action in mode_dict[int(mode[2])]:
                     return True
+    return False
+"""
+NEED TO FIX
+MAC AND RBAC SHOULD WORK TOGETHER BUT DAC USES DIFFERENT NOTATION FOR ACTION AS WELL AS DIFFERENT ACTION LIST
+"""
+def composite_rule(user,file_path,action):
+    if RBAC(user,file_path,action) and MAC(user,file_path) and DAC(user,file_path,action):
+        return True
     return False
